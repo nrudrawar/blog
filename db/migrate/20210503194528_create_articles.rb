3bafcb87415c7a122256fcb57930e8d564c65253
Nitin Rudrawar <nitin.rudrawar@gmail.com>
2021-05-03 20:59:22 +0100

    Lockfile and schema

--- db/migrate/20210503194528_create_articles.rb
+++ db/migrate/20210503194528_create_articles.rb
@@ -2,7 +2,7 @@ class CreateArticles < ActiveRecord::Migration[6.1]
   def change
     create_table :articles do |t|
       t.string :title
-      t.test :body
+      t.text :body
 
       t.timestamps
     end
